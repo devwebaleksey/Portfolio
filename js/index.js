93cd31fda62051648461b538cbbcaba54b19c0fd
devwebaleksey <111435710+devwebaleksey@users.noreply.github.com>
2022-08-22 19:54:56 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(function() {
+
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        blockId = $(this).data('scroll');
+        blockOffset = $(blockId).offset().top;
+
+        console.log(blockOffset);
+
+        $("html, body").animate({
+            scrollTop: blockOffset -50
+        }, 700);
+    });
+
+    $("[data-modal]").on("click", function(event){
+        event.preventDefault();
+
+        $(modal_resume).addClass('show');
+
+    });
+
+    $(modal_resume).on("click", function() {
+        $(modal_resume).removeClass('show');
+    });
+
+});
